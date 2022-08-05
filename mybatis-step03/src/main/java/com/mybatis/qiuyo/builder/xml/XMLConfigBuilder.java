219e882d9144ffa86d0376fdbda666a50692aca1
lqy9090 <68727327+lqy9090@users.noreply.github.com>
2022-08-05 11:44:35 +0800

    step03 v1.0 Resources

--- mybatis-step03/src/main/java/com/mybatis/qiuyo/builder/xml/XMLConfigBuilder.java
+++ mybatis-step03/src/main/java/com/mybatis/qiuyo/builder/xml/XMLConfigBuilder.java
@@ -1,6 +1,9 @@
 package com.mybatis.qiuyo.builder.xml;
 
 import com.mybatis.qiuyo.builder.BaseBuilder;
+import com.mybatis.qiuyo.io.Resources;
+import com.mybatis.qiuyo.mapping.MappedStatement;
+import com.mybatis.qiuyo.mapping.SqlCommandType;
 import com.mybatis.qiuyo.session.Configuration;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -9,8 +12,13 @@ import org.dom4j.io.SAXReader;
 import org.xml.sax.InputSource;
 
 
+import java.io.IOException;
 import java.io.Reader;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @Author: qiuyi
@@ -41,10 +49,48 @@ public class XMLConfigBuilder extends BaseBuilder {
         return configuration;
     }
 
-    private void mapperElement(Element mappers){
+    private void mapperElement(Element mappers) throws IOException, DocumentException, ClassNotFoundException {
         List<Element> mapperList = mappers.elements("mapper");
         for (Element e : mapperList) {
             String resource = e.attributeValue("resource");
+            Reader reader = Resources.geResourceAsReader(resource);
+            SAXReader saxReader = new SAXReader();
+            Document document = saxReader.read(new InputSource(reader));
+            Element root = document.getRootElement();
+            //命名空间
+            String namespace = root.attributeValue("namespace");
+
+            //SELECT
+            List<Element> selectNodes = root.elements("select");
+            for (Element node : selectNodes) {
+                String id = node.attributeValue("id");
+                String paramType = node.attributeValue("paramType");
+                String resultType = node.attributeValue("resultType");
+                String sql = node.getText();
+
+                //占位符 ? 匹配
+                HashMap<Integer, String> parameter = new HashMap<>();
+                Pattern pattern = Pattern.compile("(#\\{(.*?)})");
+                Matcher matcher = pattern.matcher(sql);
+                for (int i = 1; matcher.find(); i++) {
+                    String g1 = matcher.group(1);
+                    String g2 = matcher.group(2);
+                    parameter.put(i, g2);
+                    sql = sql.replace(g1, "?");
+                }
+
+                String msId = namespace + "." + id;
+                String nodeName = node.getName();
+                SqlCommandType.valueOf(nodeName.toUpperCase(Locale.ENGLISH));
+
+                MappedStatement mappedStatement = new MappedStatement();
+                //添加解析sql
+                configuration.addMappedStatement(mappedStatement);
+            }
+
+            //注册mapper映射器
+            //这里解析完，调用configuration去注册mapper
+            configuration.addMapper(Resources.classForName(namespace));
 
         }
     }
